3a91aa65d8d427ef61484d6991eed4d6a33d403b
Walik23 <pruxodko2005@gmail.com>
2024-02-26 02:48:12 +0200

    FMT8

--- main.go
+++ main.go
@@ -11,24 +11,19 @@ type currentTime struct {
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
-
 	t := time.Now()
 	rfc3339 := currentTime{Time: t.Format(time.RFC3339)}
 
 	w.Header().Set("Content-Type", "application/json")
 	result, err := json.Marshal(rfc3339)
-
 	if err != nil {
 		panic(err)
 	}
 
 	w.Write(result)
-
 }
 
 func main() {
-
 	http.HandleFunc("/time", api)
 	panic(http.ListenAndServe(":8795", nil))
-
 }
